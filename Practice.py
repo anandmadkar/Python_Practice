bd141995b49834712b8c5e4f80ad1d61b90d5234
Anand Madkar <72975467+anandmadkar@users.noreply.github.com>
2023-08-12 16:54:22 +0530

    Add files via upload

---
+++ Practice.py
@@ -0,0 +1,85 @@
+
+# # # -----------------Input and Replace Function Example-----------------------------------
+
+# # # This you can use for writing a mail with one template for sending bulk mails at once.
+
+# letter = '''Good Morning!'''
+# # This is to inform you <name> that it is completed and evaluated on <date>Omkar'''
+
+# name =input("Enter your Name\n")
+# Date =input("Enter the date\n")
+
+# letter = letter.replace("<name>",name)
+# letter = letter.replace("<date>",Date)
+# print(letter)
+
+
+# # # ------------------Input Function used in the list -----------------------------
+
+# # # Write a program to create a list of 6 fruits by user input.
+
+# l1 = input("Enter the name of fruit\n", )
+# l2 = input("Enter the name of fruit\n", )
+# l3 = input("Enter the name of fruit\n", )
+# l4 = input("Enter the name of fruit\n", )
+# l5 = input("Enter the name of fruit\n", )
+# l6 = input("Enter the name of fruit\n", )
+
+# list = (l1, l2, l3, l4, l5, l6)
+
+# print(list)
+
+# # # # Write a program to create a list of student marks by user input.
+
+# m1 = int(input("Enter the marks of  student\n", ))
+# m2 = int(input("Enter the marks of  student\n", ))
+# m3 = int(input("Enter the marks of  student\n", ))
+# m4 = int(input("Enter the marks of  student\n", ))
+# m5 = int(input("Enter the marks of  student\n", ))
+# m6 = int(input("Enter the marks of  student\n", ))
+
+# markslist = [m1, m2, m3, m4, m5, m6]
+
+# print(type(markslist))
+
+# markslist.sort()
+
+# print(markslist)
+
+
+# # ---------------------------------------------------------------------------------------------------------------------------------------------------------
+# # Example of Dictionary ---
+
+# myDict = {
+#     "Pankha" : "Fan",
+#     "Dabba" : "Tiffin",
+#     "Vastu" : "Item"
+# }
+
+# print("Options are ", myDict.keys())
+
+# a = input("Enter the Hindi word\n")
+
+# print("The meaning of Hindi word is", myDict[a])
+
+# # ----------------------------------------------------------------------------------------------------------------------------------------
+
+# # Example from Practice Set
+
+# # Example on Sets
+
+# num1 = int(input("Enter number 1\n"))
+# num2 = int(input("Enter number 2\n"))
+# num3 = int(input("Enter number 3\n"))
+# num4 = int(input("Enter number 4\n"))
+# num5 = int(input("Enter number 5\n"))
+# num6 = int(input("Enter number 6\n"))
+# num7 = int(input("Enter number 7\n"))
+# num8 = int(input("Enter number 8\n"))
+
+# a = {num1,num2,num3,num4,num5,num6,num7,num8}
+
+# print(a)
+
+# # # ---------------------------------------------------------------------------------------------------------------------------------------
+
